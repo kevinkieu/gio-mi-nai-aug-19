ef65b90693f041e319eaf3d39af51624a9aa469d
kevinkieu <76687919+kevinkieu@users.noreply.github.com>
2024-08-23 11:56:44 +0700

    gemini.py

--- api/gemini.py
+++ api/gemini.py
@@ -9,6 +9,7 @@ genai.configure(api_key=GOOGLE_API_KEY[0])
 
 model_usual = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
+    stream=True,
     generation_config=generation_config,
     safety_settings=safety_settings)
 
