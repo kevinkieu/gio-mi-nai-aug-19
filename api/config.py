3e4ac2a39eb885880b633cbaf391182c7664fd2e
kevinkieu <76687919+kevinkieu@users.noreply.github.com>
2024-08-23 17:26:36 +0700

    config.py

--- api/config.py
+++ api/config.py
@@ -62,7 +62,7 @@ generation_config = {
     "temperature": 1.15,
     "top_p": 0.95,
     "top_k": 61,
-    "max_output_tokens": 3200,
+    "max_output_tokens": 2708,
 }
 
 """ read https://ai.google.dev/api/rest/v1/HarmCategory """
